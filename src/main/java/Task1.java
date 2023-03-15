ffb67bf1100b64de14a7370cbb795c4eee9643d5
AnitaWolf <anito4ka83@yandex.com>
2023-03-15 14:26:43 +0100

    refactor

---
+++ src/main/java/Task1.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+    /**
+     * Дан целочисленный массив nums,
+     * отсортированный в порядке неубывания,
+     * удалите дубликаты на месте таким образом,
+     * чтобы каждый уникальный элемент появлялся
+     * только один раз. Относительный порядок элементов
+     * должен быть сохранен неизменным.
+
+     */
+    public static int findLengthOfSortedPartOfArray(ArrayList<Integer> array) {
+
+        int firstPointer = 0;
+        int secondPointer = 0;
+        int size = array.size();
+        while (firstPointer < size && secondPointer < size) {
+            if (array.get(firstPointer) == array.get(secondPointer)) {
+                secondPointer++;
+            } else {
+                firstPointer++;
+                array.set(firstPointer,array.get(secondPointer));
+            }
+        }
+        firstPointer++;
+        return firstPointer;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(List.of(0,0,1,1,1,2,2,3,3,4));
+        System.out.println("Length of sorted part: "+findLengthOfSortedPartOfArray(list));
+
+    }
+}
---
+++ src/main/java/Task2.java
@@ -0,0 +1,39 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task2 {
+
+    /**
+     * Task 2.
+     * Given a non-empty array of integers nums,
+     * every element appears twice except for one.
+     * Find that single one.
+     * Задание 2.
+     * Дан непустой массив целых чисел nums,
+     * каждый элемент появляется дважды,
+     * за исключением одного.
+     * Найди этот единственный элемент.
+     */
+    public static void main(String[] args) {
+        int[] arr = {6, 8, 3, 8, 6};
+        System.out.println(findOfUniqueElement(arr));
+    }
+
+    public static int findOfUniqueElement(int[] array) {
+        int uniqueElement = 0;
+        for (int i = 0; i < array.length; i++) {
+            int count = 0;
+            for (int j = i; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                uniqueElement = array[i];
+                break;
+            }
+        }
+        return uniqueElement;
+    }
+}
